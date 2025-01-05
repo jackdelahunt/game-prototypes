58c3db5bb81bddbdd970858e68e220c0384f3475
jackdelahunt <jdealhunt@gmail.com>
2025-01-05 06:02:37 +0000

    macos build

---
+++ game4/build.sh
@@ -0,0 +1,5 @@
+mkdir -p build
+
+./bin/sokol-shdc-macos-arm -i src/shaders/basic_shader.glsl -o src/shaders/basic_shader.odin -l metal_macos -f sokol_odin
+
+odin build src -debug -show-timings -o:none -out:build/entry.exe
