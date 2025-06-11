03226bacb790ef9f87992708e8fc0d7307eacc04
Jack Delahunt <jdealhunt@gmail.com>
2025-06-11 12:29:55 +0100

    Update build.bat

--- game2/build.bat
+++ game2/build.bat
@@ -2,6 +2,8 @@
 
 .\bin\sokol-shdc-win.exe -i src\shaders\basic_shader.glsl -o src\shaders\basic_shader.odin -l hlsl4 -f sokol_odin
 
+if not exist build mkdir build
+
 odin build src -debug -show-timings -o:none -extra-linker-flags:"/DEBUG:FULL" -out:build\entry.exe
 if %errorlevel% neq 0 exit /b %errorlevel%
 
