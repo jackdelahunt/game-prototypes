698d4a7f87ea65ed9bc2e50ae09cc8f6c982fc81
Jack Delahunt <jdealhunt@gmail.com>
2025-08-09 16:01:04 +0100

    shooting over the network

--- game11/src/main.cpp
+++ game11/src/main.cpp
@@ -13,26 +13,36 @@
 #include <mutex>
 #include <queue>
 
-// Total: 17:00
-// Started: 12:30
+// Total: 18:30
+// Started: 14:00
 
 #define MAX_ENTITIES 2000
+#define MAX_BUBBLES 10
 #define NETWORK_DELAY_MS 50
 #define SERVER_ID 1
 
+enum Team {
+    TEAM_RED,
+    TEAM_BLUE
+};
+
+// @entity
 struct Entity {
     // meta
     u64 flags;
+    u32 id;
     u32 owner;
 
     // base
     v3 position;
     v3 size;
-    v3 rotation;
     v3 velocity;
 
     // rendering
-    v4 colour;
+    Color colour;
+
+    // bullet
+    u32 bullet_origin;
 };
 
 typedef HSteamNetConnection ConnectionId;
@@ -42,6 +52,7 @@ enum NetworkMessageType {
     CLIENT_CONNECTED,
     SPAWN_ENTITY,
     SYNC_ENTITY,
+    DELETE_ENTITY,
 }; 
 
 struct NetworkMessage {
@@ -52,11 +63,13 @@ struct NetworkMessage {
         u32 client_connected;
         Entity spawn_entity;
         Entity sync_entity;
+        u32 delete_entity;
     };
 }; 
 
 enum EntityFlags {
     EF_PLAYER           = 1 << 0,
+    EF_BULLET           = 1 << 1,
     EF_DELETE           = 1 << 16,
 };
 
@@ -65,6 +78,7 @@ struct NetworkQueue {
     std::queue<Slice<u8>> messages;
 };
 
+// @server
 struct Server {
     inline static Server *instance = NULL; // used to keep server instance around for network callbacks
 
@@ -80,6 +94,7 @@ struct Server {
     NetworkQueue in_queue;
 };
 
+// @client
 struct Client {
     inline static Client *instance = NULL; // used to keep client instance around for network callbacks
 
@@ -92,9 +107,13 @@ struct Client {
     NetworkQueue in_queue;
 };
 
+// @state
 struct State {
     u32 id;
     f64 time;
+
+    const char *title;
+    v2 window_size;
 
     Server server;
     Client client;
@@ -111,7 +130,13 @@ void update_entities(f32 delta_time);
 void draw_entities(f32 delta_time);
 void physics(f32 delta_time);
 
-Entity *spawn_entity(Entity entity);
+u32 new_entity_id();
+Entity *spawn_entity_local(Entity entity);
+void spawn_entity_on_server(Entity entity);
+void delete_entity_local(u32 id);
+void delete_entity_on_server(u32 id);
+Entity *get_entity_with_id(u32 id);
+bool entities_overlap(Entity *a, Entity *b);
 
 void network_queue_push(NetworkQueue *network_queue, Slice<u8> message);
 bool network_queue_pop(NetworkQueue *network_queue, Slice<u8> *out_message);
@@ -136,30 +161,31 @@ void client_send_to_server(Client *client, Slice<u8> message);
 
 void networking_debug_callback(ESteamNetworkingSocketsDebugOutputType type, const char *message);
 
+v2 v2_cast(Vector2 v);
+v3 v3_cast(Vector2 v);
+v3 v3_cast(Vector3 v);
+
+// @main
 int main() {
-    const char *window_title = NULL;
+    state.title = is_server() ? "Game11 [Server]" : "Game11 [Client]";
+    state.window_size = v2{1080, 720};
 
 #ifdef SERVER
-    server_run(&state.server);
-    window_title = "Game11 [Server]";
     state.id = SERVER_ID;
+    server_run(&state.server);
 #endif
 
 #ifdef CLIENT
     client_run(&state.client);
-    window_title = "Game11 [Client]";
 #endif
 
     srand(time(NULL));
 
-    InitWindow(1080, 720, window_title);
+    InitWindow(i32(state.window_size.x), i32(state.window_size.y), state.title);
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        f32 delta_time = 0;
-
-        if (is_client()) {
-        }
+        f32 delta_time = GetFrameTime();
 
         update(delta_time);
         physics(delta_time);
@@ -167,7 +193,6 @@ int main() {
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
@@ -196,6 +221,15 @@ void update_network() {
             NetworkMessage *message = (NetworkMessage *) bytes.ptr;
             on_server_receive(message);
             slice_free(bytes);
+        }
+
+        for (Entity &entity : state.entities) {
+            if (entity.owner != SERVER_ID) {
+                continue;
+            }
+    
+            NetworkMessage message = NetworkMessage{.type = SYNC_ENTITY, .sync_entity = entity};
+            server_send_to_all_clients(&state.server, bytes_from_ptr(&message));
         }
     }
 
@@ -227,48 +261,60 @@ void on_server_receive(NetworkMessage *message) {
             // 3. The player entity is spawn on all clients and is owned by the new client
             // - 09/08/25
 
-            ConnectionId id = message->client_connected;
+            ConnectionId connection_id = message->client_connected;
 
             { // assign client id
-                NetworkMessage message = NetworkMessage{.type = ASSIGN_CLIENT_ID, .assign_client_id = id};
-                server_send_to_client(&state.server, bytes_from_ptr(&message), id);
-                printf("New client connected, assigning id of: %u\n", id);
+                NetworkMessage message = NetworkMessage{.type = ASSIGN_CLIENT_ID, .assign_client_id = connection_id};
+                server_send_to_client(&state.server, bytes_from_ptr(&message), connection_id);
+                printf("New client connected, assigning id of: %u\n", connection_id);
             }
 
             { // spawn any entities on new client
                 for (Entity &entity : state.entities) {
                     NetworkMessage message = NetworkMessage{.type = SPAWN_ENTITY, .spawn_entity = entity};
-                    server_send_to_client(&state.server, bytes_from_ptr(&message), id);
+                    server_send_to_client(&state.server, bytes_from_ptr(&message), connection_id);
                 }
             }
 
             { // spawn new player entity on all clients
                 Entity new_player = Entity {
                     .flags = EF_PLAYER,
-                    .owner = id,
+                    .id = new_entity_id(),
+                    .owner = connection_id,
                     .position = {50, 50, 0},
                     .size = {50, 50},
-                    .colour = {1, 0, 0, 1}
+                    .colour = RED 
                 };
 
-                spawn_entity(new_player);
+                spawn_entity_local(new_player);
 
                 NetworkMessage message = NetworkMessage{.type = SPAWN_ENTITY, .spawn_entity = new_player};
                 server_send_to_all_clients(&state.server, bytes_from_ptr(&message));
             }
         } break;
         case SPAWN_ENTITY: {
-            spawn_entity(message->spawn_entity);
+            Entity entity = message->spawn_entity;
+            entity.id = new_entity_id();
+
+            spawn_entity_local(entity);
+            NetworkMessage message = NetworkMessage{.type = SPAWN_ENTITY, .spawn_entity = entity};
+            server_send_to_all_clients(&state.server, bytes_from_ptr(&message));
         } break;
         case SYNC_ENTITY: {
-            for (Entity &entity : state.entities) {
-                if (entity.owner == message->sync_entity.owner) {
-                    entity = message->sync_entity;
-                    NetworkMessage message = NetworkMessage{.type = SYNC_ENTITY, .sync_entity = entity};
-                    server_send_to_all_clients(&state.server, bytes_from_ptr(&message), entity.owner);
-                    break;
-                }
+            Entity *entity = get_entity_with_id(message->sync_entity.id);
+            if (entity != NULL && entity->owner != SERVER_ID) {
+                *entity = message->sync_entity;
+
+                NetworkMessage message = NetworkMessage{.type = SYNC_ENTITY, .sync_entity = *entity};
+                server_send_to_all_clients(&state.server, bytes_from_ptr(&message), entity->owner);
             }
+        } break;
+        case DELETE_ENTITY: {
+            u32 id = message->delete_entity;
+
+            delete_entity_local(id);
+            NetworkMessage message = NetworkMessage{.type = DELETE_ENTITY, .delete_entity = id};
+            server_send_to_all_clients(&state.server, bytes_from_ptr(&message));
         } break;
         default: {
             logln("WARNING unknown message sent");
@@ -283,15 +329,16 @@ void on_client_receive(NetworkMessage *message) {
             printf("Assigned id of: %u\n", state.id);
         } break;
         case SPAWN_ENTITY: {
-            spawn_entity(message->spawn_entity);
+            spawn_entity_local(message->spawn_entity);
         } break;
         case SYNC_ENTITY: {
-            for (Entity &entity : state.entities) {
-                if (entity.owner == message->sync_entity.owner) {
-                    entity = message->sync_entity;
-                    break;
-                }
+            Entity *entity = get_entity_with_id(message->sync_entity.id);
+            if (entity != NULL) {
+                *entity = message->sync_entity;
             }
+        } break;
+        case DELETE_ENTITY: {
+            delete_entity_local(message->delete_entity);
         } break;
         default: {
             logln("WARNING unknown message sent");
@@ -301,27 +348,71 @@ void on_client_receive(NetworkMessage *message) {
 
 void update_entities(f32 delta_time) {
     for (Entity &entity : state.entities) {
+        if (entity.owner != state.id) {
+            continue;
+        }
+
         if (BIT_SET(entity.flags, EF_PLAYER)) {
-            if (entity.owner != state.id) {
-                continue;
+            { // movement
+                f32 move_speed = 10;
+    
+                if (IsKeyDown(KEY_A)) {
+                    entity.position.x -= move_speed;
+                }
+    
+                if (IsKeyDown(KEY_D)) {
+                    entity.position.x += move_speed;
+                }
+    
+                if (IsKeyDown(KEY_W)) {
+                    entity.position.y -= move_speed;
+                }
+    
+                if (IsKeyDown(KEY_S)) {
+                    entity.position.y += move_speed;
+                }
             }
 
-            f32 speed = 10;
+            // firing bullets
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                f32 bullet_speed = 500;
+                v3 direction =  norm(v3_cast(GetMousePosition()) - entity.position);
 
-            if (IsKeyDown(KEY_A)) {
-                entity.position.x -= speed;
+                Entity bullet = Entity {
+                    .flags = EF_BULLET,
+                    .owner = SERVER_ID,
+                    .position = entity.position,
+                    .size = v3{20, 20, 0},
+                    .velocity = v3{direction.x, direction.y, 0} * bullet_speed,
+                    .colour = RED,
+                    .bullet_origin = entity.id,
+                };
+
+                spawn_entity_on_server(bullet);
             }
 
-            if (IsKeyDown(KEY_D)) {
-                entity.position.x += speed;
-            }
+            // bullet collision detection
+            for (Entity &other : state.entities) {
+                if (!BIT_SET(other.flags, EF_BULLET)) {
+                    continue;
+                }
 
-            if (IsKeyDown(KEY_W)) {
-                entity.position.y -= speed;
-            }
+                if (other.bullet_origin == entity.id) {
+                    continue;
+                }
 
-            if (IsKeyDown(KEY_S)) {
-                entity.position.y += speed;
+                if (!entities_overlap(&entity, &other)) {
+                    continue;
+                }
+
+                delete_entity_on_server(other.id);
+            }
+        }
+
+        if (BIT_SET(entity.flags, EF_BULLET)) {
+            if ((entity.position.x < -entity.size.x || entity.position.x > state.window_size.x + entity.size.x) ||
+                (entity.position.y < -entity.size.y || entity.position.y > state.window_size.y + entity.size.y)) {
+                SET_BIT(entity.flags, EF_DELETE);
             }
         }
     }
@@ -329,7 +420,12 @@ void update_entities(f32 delta_time) {
 
 void draw_entities(f32 delta_time) {
     for (Entity &entity : state.entities) {
-        DrawRectangle(i32(entity.position.x), i32(entity.position.y), i32(entity.size.x), i32(entity.size.y), BLUE);
+        if (BIT_SET(entity.flags, EF_PLAYER)) {
+            DrawRectangle(i32(entity.position.x), i32(entity.position.y), i32(entity.size.x), i32(entity.size.y), entity.colour);
+        }
+        else {
+            DrawCircle(i32(entity.position.x), i32(entity.position.y), i32(entity.size.x), entity.colour);
+        }
     }
 }
 
@@ -345,45 +441,72 @@ bool point_collision(v3 point, v3 collider_position, v3 collider_size) {
     );
 }
 
-struct CubeCollision {
-    bool collision;
-    v3 overlap;
-    v3 distance;
-};
-
-CubeCollision cube_collision(v3 a_position, v3 a_size, v3 b_position, v3 b_size) {
-    v3 distance = b_position - a_position;
-    v3 distance_abs = v3{ABS(distance.x), ABS(distance.y), ABS(distance.z)};
-    v3 distance_for_collision = (a_size + b_size) * 0.5; 
-
-    bool collision = distance_for_collision.x >= distance_abs.x && distance_for_collision.y >= distance_abs.y && distance_for_collision.z >= distance_abs.z;
-    v3 overlap = distance_for_collision - distance_abs;
-
-    return CubeCollision {
-        .collision = collision,
-        .overlap = overlap,
-        .distance = distance
-    };
-}
-
 void physics(f32 delta_time) {
-    for (int i = 0; i < state.entities.len; i++) {
-        Entity* entity = &state.entities[i];
-
-        const f32 MAX_SPEED = 50;
-        if (length(entity->velocity) > MAX_SPEED) {
-            entity->velocity = norm(entity->velocity) * MAX_SPEED;
-        }
-
-        entity->position += entity->velocity * delta_time;
+    for (Entity &entity : state.entities) {
+        entity.position += entity.velocity * delta_time;
     }
 }
 
-Entity *spawn_entity(Entity entity) {
+u32 new_entity_id() {
+    static u32 id = 0;
+    return id++;
+}
+
+Entity *spawn_entity_local(Entity entity) {
     Entity *ptr = push(&state.entities);
     *ptr = entity;
 
     return ptr;
+}
+
+void delete_entity_local(u32 id) {
+    for (i64 i = 0; i < state.entities.len; i++) {
+        Entity *entity = &state.entities[i];
+
+        if (entity->id == id) {
+            swap_remove(&state.entities, i);
+            return;
+        }
+    }
+}
+
+void spawn_entity_on_server(Entity entity) {
+    NetworkMessage message = NetworkMessage{.type = SPAWN_ENTITY, .spawn_entity = entity};
+    client_send_to_server(&state.client, bytes_from_ptr(&message));
+}
+
+void delete_entity_on_server(u32 id) {
+    NetworkMessage message = NetworkMessage{.type = DELETE_ENTITY, .delete_entity = id};
+    client_send_to_server(&state.client, bytes_from_ptr(&message));
+}
+
+Entity *get_entity_with_id(u32 id) {
+    for (Entity &entity : state.entities) {
+        if (entity.id == id) {
+            return &entity;
+        }
+    }
+
+    return NULL;
+}
+
+bool entities_overlap(Entity *a, Entity *b) {
+    // Compute edges
+    float a_min_x = a->position.x;
+    float a_max_x = a->position.x + a->size.x;
+    float a_min_y = a->position.y;
+    float a_max_y = a->position.y + a->size.y;
+
+    float b_min_x = b->position.x;
+    float b_max_x = b->position.x + b->size.x;
+    float b_min_y = b->position.y;
+    float b_max_y = b->position.y + b->size.y;
+
+    // Check for separation along x and y axes
+    bool overlapX = (a_min_x < b_max_x) && (a_max_x > b_min_x);
+    bool overlapY = (a_min_y < b_max_y) && (a_max_y > b_min_y);
+
+    return overlapX && overlapY;
 }
 
 void network_queue_push(NetworkQueue *network_queue, Slice<u8> message) {
@@ -729,4 +852,16 @@ void networking_debug_callback(ESteamNetworkingSocketsDebugOutputType type, cons
         printf("[NETWORK]: fatal error\n");
         BREAKPOINT;
     }
+}
+
+v2 v2_cast(Vector2 v) {
+    return v2{v.x, v.y};
+}
+
+v3 v3_cast(Vector2 v) {
+    return v3{v.x, v.y, 0};
+}
+
+v3 v3_cast(Vector3 v) {
+    return v3{v.x, v.y, v.z};
 }
